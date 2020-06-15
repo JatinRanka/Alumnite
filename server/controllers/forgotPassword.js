60155ff3591e320f73634b64a7cd944c314f1be6
JatinRanka <37477429+JatinRanka@users.noreply.github.com>
2020-06-15 22:47:52 +0530

    Added new files :package:

---
+++ server/controllers/forgotPassword.js
@@ -0,0 +1,3 @@
+module.exports = function forgotPassword(){
+    return "in forgot pwd"
+}
---
+++ server/controllers/index.js
@@ -0,0 +1,5 @@
+const forgotPassword = require('./forgotPassword');
+
+module.exports = {
+    forgotPassword
+}
