635d4e7cb5761af7ea3f9ee11a5e0590b4ef0384
JatinRanka <37477429+JatinRanka@users.noreply.github.com>
2020-07-10 15:44:34 +0530

    Updated error messages :pencil:

--- server/middleware/alumniAuth.js
+++ server/middleware/alumniAuth.js
@@ -5,13 +5,13 @@ var alumniAuth = (req, res, next) => {
     var token = req.header('x-auth');
 
     if(!token){
-        res.status(400).send({'err': 'User not logged in.'})
+        res.status(400).send({'err': 'Token not found (user not logged in).'})
     }
 
     Alumni.findByToken(token)
         .then((alumni) => {
             if(!alumni) {
-                res.status(400).send({'err': "Invalid credentials."});
+                res.status(400).send({'err': "Token invalid."});
             }
             req.alumni = alumni;
             req.token = token;
